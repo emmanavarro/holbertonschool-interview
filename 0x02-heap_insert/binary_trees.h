e9cca7c452edc463741e13ac8c823493d904cc36
emmanavarro <elnavarro55@gmail.com>
2020-12-21 15:09:58 -0500

    Header file added

---
+++ 0x02-heap_insert/binary_trees.h
@@ -0,0 +1,40 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Basic Binary Tree */
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+/* Max Binary Heap */
+typedef struct binary_tree_s heap_t;
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* Function to print a binary tree */
+void binary_tree_print(const binary_tree_t *);
+
+/* Function to create a binary tree node */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Function to insert a value in a Max Binary Heap*/
+heap_t *heap_insert(heap_t **root, int value);
+
+#endif /* _BINARY_TREES_H_ */
