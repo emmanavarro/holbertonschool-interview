e1694c92eaf09ae5441fd63abedbe303c35bcf86
Emma Navarro <elnavarro55@gmail.com>
2021-11-11 22:37:40 -0500

    [Add] Star Wars Characters

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,31 +1,24 @@
 #!/usr/bin/node
 
-const movId = process.argv[2];
-
-const url = `https://swapi-api.hbtn.io/api/films/${movId}`;
-
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, async (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const listChar = JSON.parse(body).characters;
-    for (const urlCharacter of listChar) {
-      console.log(await getNameChar(urlCharacter));
-    }
-  }
-});
-
-function getNameChar (apiCharacter) {
-  const promise = new Promise((resolve, reject) => {
-    request(apiCharacter, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body).name);
-      }
-    });
+const start = function () {
+  request(url, function (error, response, body) {
+    if (error) throw error;
+    end(JSON.parse(body).characters, 0);
   });
-  return promise;
-}
+};
+
+const end = function (characters, i) {
+  if (characters.length === i) {
+    return;
+  }
+  request(characters[i], function (error, response, body) {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    end(characters, ++i);
+  });
+};
+
+start();
