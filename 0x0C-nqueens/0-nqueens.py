ee1d6c10d914e1e3baab68899d1f606b635c95d8
emmanavarro <elnavarro55@gmail.com>
2022-03-17 22:02:38 -0500

    fix

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print('Usage: nqueens N')
         sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except BaseException:
+    n = sys.argv[1]
+    if n.isdigit() is not True:
         print('N must be a number')
         sys.exit(1)
+    n = int(n)
     if n < 4:
         print('N must be at least 4')
         sys.exit(1)
