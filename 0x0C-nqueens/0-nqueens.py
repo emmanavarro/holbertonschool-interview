c3177465718cd991918df5a2a6bcbcd1e1dcf065
emmanavarro <elnavarro55@gmail.com>
2022-03-17 22:56:56 -0500

    Exec permissions

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,38 +1,52 @@
 #!/usr/bin/python3
-"""
-Program that solves the N queens problem
-"""
+"""program that solves the N queens problem."""
+
+from sys import argv, exit
 
 
-import sys
+if len(argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+N = argv[1]
+
+try:
+    N = int(N)
+except ValueError:
+    print("N must be a number")
+    exit(1)
+
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
+
+solution = []
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-
-def backtracking(N, i=0, a=[], b=[], c=[]):
-    """Generates backtracking solutions for N queens problem"""
-    if i < N:
-        for j in range(N):
-            if j not in a and i+j not in b and i-j not in c:
-                for solution in backtracking(N, i+1, a+[j], b+[i+j], c+[i-j]):
-                    yield solution
+def nqueens(row, N, solution):
+    """The program should print any possible solution"""
+    if (row == N):
+        print(solution)
     else:
-        yield a
+        for col in range(N):
+            position = [row, col]
+            if validposition(solution, position):
+                solution.append(position)
+                nqueens(row + 1, N, solution)
+                solution.remove(position)
 
-for solution in backtracking(N):
-    answer = [[col, row] for col, row in enumerate(solution)]
-    print(answer)
+
+def validposition(solution, position):
+    """validate horizontal and diagonal position of queens"""
+    for queen in solution:
+        if queen[1] == position[1]:
+            return False
+        # descending diagonal
+        if (queen[0] - queen[1]) == (position[0] - position[1]):
+            return False
+        # ascending diagonal
+        if (queen[0] + queen[1]) == (position[0] + position[1]):
+            return False
+    return True
+
+nqueens(0, N, solution)
