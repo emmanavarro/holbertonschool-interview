8705cb14b545224afff5765f98bce0024c943305
emmanavarro <elnavarro55@gmail.com>
2022-06-23 21:56:33 -0500

    update

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -17,18 +17,20 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
+    newVal = total + 1
+    store = {0: 0}
 
-    i, ncoins = (0, 0)
-    cpy_total = total
-    len_coins = len(coins)
+    for i in range(1, total + 1):
+        store[i] = newVal
 
-    while(i < len_coins and cpy_total > 0):
-        if (cpy_total - coins[i]) >= 0:
-            cpy_total -= coins[i]
-            ncoins += 1
-        else:
-            i += 1
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
 
-    check = cpy_total > 0 and ncoins > 0
-    return -1 if check or ncoins == 0 else ncoins
+            store[i] = min(store[current] + 1, store[i])
+
+    if store[total] == total + 1:
+        return -1
+
+    return store[total]
